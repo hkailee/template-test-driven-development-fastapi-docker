e2d6375b8d2a96f52329db197ed8d0c6d19d6835
leehk <leehongkai@gmail.com>
2024-03-06 15:42:46 +0800

    black and ruff fixed

--- viz2/app/models/pydantic.py
+++ viz2/app/models/pydantic.py
@@ -7,9 +7,10 @@ from pydantic import BaseModel, AnyHttpUrl
 class SummaryPayloadSchema(BaseModel):
     url: AnyHttpUrl
 
+
 class SummaryResponseSchema(SummaryPayloadSchema):
     id: int
 
+
 class SummaryUpdatePayloadSchema(SummaryPayloadSchema):
     summary: str
-    
--- viz2/tests/test_summaries.py
+++ viz2/tests/test_summaries.py
@@ -2,7 +2,7 @@
 
 
 import json
-import pytest
+
 
 def test_create_summary(test_app_with_db):
     response = test_app_with_db.post(
@@ -45,8 +45,6 @@ def test_read_summary(test_app_with_db):
     assert response_dict["created_at"]
 
 
-
-
 def test_read_all_summaries(test_app_with_db):
     response = test_app_with_db.post(
         "/summaries/", data=json.dumps({"url": "https://foo.bar"})
@@ -76,7 +74,7 @@ def test_remove_summary_incorrect_id(test_app_with_db):
     assert response.status_code == 404
     assert response.json()["detail"] == "Summary not found"
 
-  
+
 def test_update_summary(test_app_with_db):
     response = test_app_with_db.post(
         "/summaries/", data=json.dumps({"url": "https://foo.bar"})
@@ -85,7 +83,7 @@ def test_update_summary(test_app_with_db):
 
     response = test_app_with_db.put(
         f"/summaries/{summary_id}/",
-        data=json.dumps({"url": "https://foo.bar", "summary": "updated!"})
+        data=json.dumps({"url": "https://foo.bar", "summary": "updated!"}),
     )
     assert response.status_code == 200
 
@@ -99,7 +97,7 @@ def test_update_summary(test_app_with_db):
 def test_update_summary_incorrect_id(test_app_with_db):
     response = test_app_with_db.put(
         "/summaries/999/",
-        data=json.dumps({"url": "https://foo.bar", "summary": "updated!"})
+        data=json.dumps({"url": "https://foo.bar", "summary": "updated!"}),
     )
     assert response.status_code == 404
     assert response.json()["detail"] == "Summary not found"
@@ -111,10 +109,7 @@ def test_update_summary_invalid_json(test_app_with_db):
     )
     summary_id = response.json()["id"]
 
-    response = test_app_with_db.put(
-        f"/summaries/{summary_id}/",
-        data=json.dumps({})
-    )
+    response = test_app_with_db.put(f"/summaries/{summary_id}/", data=json.dumps({}))
     assert response.status_code == 422
     assert response.json() == {
         "detail": [
@@ -131,8 +126,6 @@ def test_update_summary_invalid_json(test_app_with_db):
                 "msg": "Field required",
                 "type": "missing",
                 "url": "https://errors.pydantic.dev/2.6/v/missing",
-            }
+            },
         ]
     }
-
-
